6ef768cd8c2249c8e72876c60cafefb182b5a111
Kam <erkamyaman35@gmail.com>
2024-06-23 04:02:03 +0300

    react-native-button

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello there! It is my first app!</Text>
+      <Button
+        title="Click Me"
+        color="#f194ff"
+        onPress={() => console.log('Button pressed!')}
+      />
       <StatusBar style="auto" />
     </View>
   );
